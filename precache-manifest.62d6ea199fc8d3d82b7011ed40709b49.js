310a1127749b8a41e7356b239e4bbca9cbb63f96
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-10-08 15:31:46 +0300

    Updates

---
+++ precache-manifest.62d6ea199fc8d3d82b7011ed40709b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cb81b219dd802b2ad51e381e4890f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ddaa0e73135254900f5a",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddaa0e73135254900f5a",
+    "url": "./static/js/main.7c1c82e3.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
